81b75dd7b90385b23044a2ecb7765e64f7999aa5
ShuhaibNC <shuhaibsaad5@gmail.com>
2023-03-19 19:34:14 +0530

    ok

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for
 from funcs import get_thanosquote
+import os
+
+port = int(os.environ.get("PORT", 5000))
 
 app = Flask(__name__)
 
@@ -9,4 +12,4 @@ def thanos():
             'index.html', 
             thanos_quote=get_thanosquote(),
             )
-app.run(host='0.0.0.0', port=80)
+app.run(debug=False, host='0.0.0.0', port=port)
