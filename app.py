532658d88c9f600a504884883c71718c43972dd6
ShuhaibNC <shuhaibsaad5@gmail.com>
2024-08-30 20:20:55 +0530

    New Theme

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, Response
 from funcs import get_thanosquote, get_img
+import requests
+import random
+import string  # Import string module
 
 app = Flask(__name__)
 
@@ -11,6 +14,17 @@ def thanos():
             imgg=get_img(), 
             )
             
+@app.route('/pass')
+def generate_password():  # Renamed function
+    adjresp = requests.get("https://gist.githubusercontent.com/hugsy/8910dc78d208e40de42deb29e62df913/raw/eec99c5597a73f6a9240cab26965a8609fa0f6ea/english-adjectives.txt")
+    adj = random.choice(adjresp.text.split('\n'))
+    nounresp = requests.get("https://raw.githubusercontent.com/hugsy/stuff/main/random-word/english-nouns.txt")
+    noun = random.choice(nounresp.text.split("\n"))
+    num = str(random.randrange(100))
+    punct = random.choice(string.punctuation)
+    passw = adj + noun + num + punct
+    return Response(passw, mimetype='text/plain')
+
 @app.route('/about')
 def about():
     return render_template(
