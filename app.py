adc603d68b6d43609f0bb324a963dad50110cedb
ShuhaibNC <shuhaibsaad5@gmail.com>
2024-08-30 14:42:54 +0530

    New

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, url_for
-from funcs import get_thanosquote
-import os
+from funcs import get_thanosquote, get_img
 
 app = Flask(__name__)
 
@@ -9,6 +8,7 @@ def thanos():
     return render_template(
             'index.html', 
             thanos_quote=get_thanosquote(),
+            imgg=get_img(), 
             )
 if __name__ == '__main__':
     app.run()
--- funcs.py
+++ funcs.py
@@ -3,3 +3,17 @@ import random
 def get_thanosquote():
     thanos_quotes = [    "I am inevitable.",    "You should have gone for the head.",    "The end is near.",    "The hardest choices require the strongest wills.",    "Dread it, run from it, destiny still arrives.",    "I ignored my destiny once, I cannot do that again.",    "I know what it's like to lose. To feel so desperately that you're right, yet to fail nonetheless.",    "In time, you will know what it's like to lose. To feel so desperately that you're right, yet to fail all the same.",    "I finally rest, and watch the sun rise on a grateful universe.",    "Fun isn't something one considers when balancing the universe. But this... does put a smile on my face.",    "I'm the only one who knows that. At least, I'm the only who has the will to act on it.",    "Your optimism is misplaced, Asgardian.",    "The universe is finite, its resources finite... if life is left unchecked, life will cease to exist.",    "I am the Titan that will change it.",    "The universe requires correction. After that, it will require someone to rule it. That someone will be me.",    "I thought by eliminating half of life, the other half would thrive, but you have shown me... that's impossible.",    "You're not the only one cursed with knowledge.",    "A small price to pay for salvation.",    "The work is done. I won. What I'm about to do... what I'm about to do, I'm gonna enjoy it. Very, very much.",    "Reality is often disappointing."]
     return random.choice(thanos_quotes)
+    
+def get_img():
+    thanos_images = [
+    "https://wallpapercave.com/wp/wp5199996.jpg",
+    'https://wallpapercave.com/wp/wp11108669.png',
+    'https://wallpapercave.com/wp/wp6650204.jpg',
+    'https://wallpapercave.com/wp/wp7661937.jpg',
+    'https://wallpapercave.com/wp/wp6009732.png',
+    'https://wallpapercave.com/wp/wp11108659.jpg',
+    'https://wallpapercave.com/wp/wp10665964.jpg',
+    'https://wallpapercave.com/wp/wp11108745.jpg',
+    
+    ]
+    return random.choice(thanos_images)
